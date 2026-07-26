b41f5c924b07d2eafade62f775f4371af7bfb5ad
Saurav Venu <souravv2006@gmail.com>
2026-07-26 16:12:51 +0530

    Add marks grading program

---
+++ MODULE_1/Q1_MARKS.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Sum            : " << a + b << endl;
+    cout << "Difference     : " << a - b << endl;
+    cout << "Multiplication : " << a * b << endl;
+    cout << "Division       : " << a / b << endl;
+    cout << "Modulus        : " << a % b << endl;
+
+    return 0;
+}
