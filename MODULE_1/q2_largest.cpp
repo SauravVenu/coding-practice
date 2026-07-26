86b87875838fd15a850047f946d71cf924042f62
Saurav Venu <souravv2006@gmail.com>
2026-07-26 16:13:27 +0530

    Add largest of two numbers program

---
+++ MODULE_1/q2_largest.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+
+    cout << " ENTER TWO NUNMBERS" << endl;
+    cin >> a >>b;
+
+    if(a>b) {
+
+        cout<< a << " is greater than " << b << endl ;
+
+    }else if (b>a) {
+
+        cout<< b << " is greater than " << a << endl ;
+
+    }else{
+
+        cout<< " Both the numbers are same";
+
+    }
+
+    return 0;
+}
