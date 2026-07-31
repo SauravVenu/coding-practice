04d503265ca510abbf45bccf190d494e22572e4a
Saurav Venu <souravv2006@gmail.com>
2026-07-31 23:57:43 +0530

    Add diamond pattern using nested loops

---
+++ MODULE_1/q11_nested_for_loops_pattern_7.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+ int n,i,j,k;
+
+int main(){
+
+    cout<< " ENTER THE NUMBER OF ROWS : ";
+    cin>>n;
+
+   for(i=1;i<=n;i++){
+    for(k=1;k<=n-i;k++){
+        cout<< " ";
+    }
+        for(j=1;j<=2*i-1;j++){
+            cout<< "*";
+        }
+    cout<< endl;
+   } 
+
+   for(i=n-1;i>=1;i--){
+    for(k=1;k<=n-i;k++){
+        cout<< " ";
+    }
+    for(j=1;j<=2*i-1;j++){
+        cout<<"*";
+    }cout<<endl;
+      
+ }
+ return 0;
+
+}
