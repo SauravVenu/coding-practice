e043ae824c12f8950e5082e4adcaa67c3f6893ca
Saurav Venu <souravv2006@gmail.com>
2026-08-09 02:46:30 +0530

    Module 2: Recursion Sum Practice

---
+++ MODULE_2/q13_recursion_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int n,ans;
+
+int print(int n){
+
+    if(n==0){
+        return 0;
+    }
+    return n + print(n-1);
+}
+
+int main(){
+
+    int ans = print(5);
+    cout << " Sum is " << ans;
+    return 0;
+
+
+}
