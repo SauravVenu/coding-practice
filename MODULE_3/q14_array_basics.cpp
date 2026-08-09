9141a725acfd4a79e4122c582e4f07105fdfa4f2
Saurav Venu <souravv2006@gmail.com>
2026-08-09 20:15:16 +0530

    Module 3: Array Basics

---
+++ MODULE_3/q14_array_basics.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+ int num [5] = {10,20,30,40,50};
+
+ cout << num[0] << endl;
+ cout << num[4] << endl;
+ num[2] = 100;
+ cout << num[2] << endl;
+
+ return 0;
+}
