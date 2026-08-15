02a2aa79a8d11125ad81e4efb1524247b9b6f25b
Saurav Venu <souravv2006@gmail.com>
2026-08-15 20:32:36 +0530

    Module 2: Prime Checking Function

---
+++ MODULE_2/q19_prime_check.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n){
+    if(n<=1){
+        return false;
+    }
+
+    for(int i=2;i<n;i++){
+        if(n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+
+    int n;
+
+    cout << " enter the number : ";
+    cin >> n;
+
+    if(isPrime(n)==true){
+        cout << " Prime";
+    }else{
+        cout << "Not Prime";
+    }
+
+    return 0;
+
+}
