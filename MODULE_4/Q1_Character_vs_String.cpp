8d11752a18607fb16e6cded3fd6951b43c6d8707
Saurav Venu <souravv2006@gmail.com>
2026-08-22 23:41:19 +0530

    Module 4: Character vs String

---
+++ MODULE_4/Q1_Character_vs_String.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    char initial = 'S';
+    string name = "Saurav";
+
+    cout << "Initial : " << initial <<endl;
+    cout << "Name : " << name <<endl;
+
+    return 0;
+}
