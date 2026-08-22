d1045ed8834f12376d018fbce61a0dd528882ef7
Saurav Venu <souravv2006@gmail.com>
2026-08-23 03:27:15 +0530

    Module 4: String Input using getline

---
+++ MODULE_4/Q5_String_Input_using_getline.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string name;
+    cout <<"enter your full name : ";
+    getline(cin,name);
+
+    cout <<"your name is : " << name;
+    return 0;
+}
