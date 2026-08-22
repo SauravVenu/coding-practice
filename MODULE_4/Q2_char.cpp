55822ae8b1df759512b0fa48dd0612db04fb9eae
Saurav Venu <souravv2006@gmail.com>
2026-08-23 02:13:06 +0530

    Module 4: char

---
+++ MODULE_4/Q2_char.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char ch = 'M';
+    cout << "original character : " << ch << endl;
+    ch='N';
+    cout << "new charaacter : " << ch;
+    return 0;
+}
