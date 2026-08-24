24128a926d6432dd54e2b82475b0c3bccb8e829b
Saurav Venu <souravv2006@gmail.com>
2026-08-24 20:53:21 +0530

    Module 4: String Traversal

---
+++ MODULE_4/Q6_String_Traversal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string word ="PROGRAM";
+    for(int i =0; i < word.length(); i++){
+        cout << word[i];
+        cout << endl;
+    }
+    return 0;
+}
