321ad83848ca6d087e19ca649a113e146acad856
Saurav Venu <souravv2006@gmail.com>
2026-08-24 21:27:16 +0530

    Module 4: String Length

---
+++ MODULE_4/Q7_String_Length.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string word = "ELEPHANT";
+    cout << word.length() << endl;
+    cout << word[0] << endl;
+    cout << word[word.length()-1];
+
+    return 0;
+}
