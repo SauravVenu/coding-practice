1fef2e57ebae0fc75a12d0acc40a1643e134ece6
Saurav Venu <souravv2006@gmail.com>
2026-08-25 19:22:37 +0530

    Module 4: Palindrome Method 1

---
+++ MODULE_4/Q9_Palindrome_method_1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string word;
+    cout << " enter the word : ";
+    cin >> word;
+
+    int left = 0;
+    int right = word.length()-1;
+    bool isPalindrome = false;
+
+    for(int i = 0; i < word.length()/2;i++){
+        if(word[left]==word[right]){
+            left++;
+            right--;
+        }
+        
+    }
+    if(word[left]==word[right]){
+        isPalindrome = true;
+    }
+
+    if(isPalindrome){
+        cout << "the word is palindrome";
+    }else{
+        cout << " the word is not a palindrome";
+    }
+  return 0;
+}
