80709220ddaa4b0bc097f906a6b969d3821bbeba
Saurav Venu <souravv2006@gmail.com>
2026-08-26 21:39:29 +0530

    Module 4: Find Substring

---
+++ MODULE_4/Q18_Find_Substring.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    
+    string word;
+    string substring;
+
+    cout <<"enter the word : ";
+    cin >>word;
+
+    cout << "enter the substring : ";
+    cin >>substring;
+
+    for(int i=0;i<word.length();i++){
+        word[i]=tolower(word[i]);
+    }
+    for(int i=0;i<substring.length();i++){
+        substring[i]=tolower(substring[i]);
+    }
+        int index = word.find(substring);
+
+    if(index!=-1){
+        cout <<"Sub string found at index "<<index;
+    }else{
+        cout << "Sub string not found";
+    }        
+    return 0;
+}
